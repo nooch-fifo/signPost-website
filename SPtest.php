ee4597498fec34f4febd8407e22b5e7fed60c361
Dominic Rego <58356156+nooch-fifo@users.noreply.github.com>
2021-02-26 16:21:54 -0500

    Add files via upload

---
+++ SPtest.php
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, intial-scale=1.0,
+        maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Sign Post | Home</title>
+    <!-- Ion Icons-->
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <!--Google Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Aldrich&display=swap" rel="stylesheet">
+    <!--Our own stylesheet-->
+    <link rel="stylesheet" href="styles33.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+    <header>
+        <div class="container">
+                <div class="nav-brand">
+									<!--<a href="http://localhost/SPtest.php" class="logo">
+						        <img src="SPLOGO2.png" alt="nav_logo">
+						      </a>-->
+                </div>
+            		<ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link current">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="http://127.0.0.1:5500/SPNJwork.html" class="nav-link">Our Work</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="http://localhost/SPcontactUS1.php" class="nav-link">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">About</a>
+                    </li>
+                </ul>
+        </div>
+    </header>
+		<main>
+    <section class="hero">
+        <div class="container">
+            <div class="main-message">
+                <h3 style="color:black;"><u>THIRD GENERATION OF QUALITY SIGN MAKING</u></h3>
+                <h1> The Sign Post </h1>
+								<p>Located on 357 Broad St, Bloomfield, NJ, 07003 </p>
+                <div class="cta">
+                    <a href="http://localhost/SPcontactUS1.html" class="btn">Contact Us Today</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="personal-experience">
+        <div class="container">
+            <div class="title-heading">
+                <h3><u>The Sign Post Guarantee </u></h3>
+                <h1> Personal Service from Experienced Professionals</h1>
+                <p>50 years of experience in conjunction with a friendly and knowledgebale attitude, we provide custom service from start to finish. </p>
+            </div>
+
+            <div class="guarantees-grid">
+                <!--grid item #1-->
+								<div class="guarantees-grid-item custom">
+											 <i class="ion-md-create"></i>
+											 <h1> Customization </h1>
+											 <p>From laser engraving to vehicle lettering, we can satisfy any of your customization needs.</p>
+									 </div>
+
+             <!--grid item #2-->
+						 <div class="guarantees-grid-item shipping">
+	 							<i class="ion-md-cube"></i>
+	 							<h1> Safe Shipping </h1>
+	 							<p>Although we are located in the tri-state area, shipping is offered throughout the continental United States.  </p>
+	 					</div>
+                <!--grid item #3-->
+								<div class="guarantees-grid-item turnaround">
+		 									<i class="ion-md-paper-plane"></i>
+		 									<h1> Fast Turnaround </h1>
+		 									<p>Whether you need one sign or one hundred signs, we can turn them around quickly and safely. </p>
+		 					</div>
+                <!--grid item #4-->
+            <!--<div class="guarantees-grid-item essential">
+                    <i class="ion-md-pulse"></i>
+                    <h1> COVID-19 Response</h1>
+                    <p> Emphasized even moreso with the current COVID-19 pandemic, our location prioritizes our worker's and customer's health and safety. Along with a clean and sanitary workplace, CDC guidelines are followed with masks being required at pickup. Thank you to all essential workers and to our healthcare heroes.</p>
+                </div> -->
+						</div>
+        </div>
+    </section>
+
+		<section class="pandemic-response">
+			<div class="container">
+				<div class="pandemic">
+						<div class="pandemic-text-box">
+							<!--<h1> From Our CEO: </h1> -->
+							<br><br><br><p>The Sign Post prioritizes our worker's and customer's health and safety above all. Along with a clean and sanitary workplace, CDC guidelines are strictly followed with masks being required at pickup.<br> Thank you to all essential workers helping us get through these difficult times.</p>
+							<i class="icon ion-md-quote"></i>
+						</div>
+						<div class="pandemic-link">
+							<img src="essentialPLS2.jpg" alt="">
+								<h1> Support Our Essential Workers!</h1><br></br>
+							<!--<a href="#" class="btn">Shop Signs</a> -->
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="social-media">
+				<div class="container">
+					<div class="social-heading">
+							<h3> VISIT US ON </h3>
+								<h1> SOCIAL MEDIA </h1>
+									<p> Stay connected with The Sign Post NJ through our Facebook or Instagram accounts. Feel free to direct message us on our social media pages for any questions or inquiries! Call us today at 973-743-5051 </p><br></br>
+
+					</div>
+					<div class="social-menu">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</section>
+</main>
+<footer>
+	<p>Copyright &copy; 2020 Sign Post NJ. </p><br>
+	<a href="http://localhost/SPtest.php">
+			<img src="logo.png" alt="" height="50" width="77">
+	</a>
+</footer>
+
+
+    <script type ="text/javascript" src="scripts.js"></script>
+</body>
+</html>
